58b0ed76a7c02c350fa92c662c7e122780659fa9
unknown <johnnyboy59000@gmail.com>
2024-11-16 15:20:31 +0100

    orthographe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>JOYEUX ANNIVERSSAIRE ÉPAVE 🎉!</h1>
+      <h1 className={styles.title}>JOYEUX ANNIVERSAIRE ÉPAVE 🎉!</h1>
       <p className={styles.description}>
         Un cadeau spécial t&apos;attend... Clique pour le découvrir !
       </p>
